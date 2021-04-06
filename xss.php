5cc48c92ac469ca1e12602ccbc04dd7c857e14c2
Wei ZHANG <yidazhang1@gmail.com>
2021-04-06 19:19:47 +0800

    Create xss.php

---
+++ xss.php
@@ -0,0 +1,10 @@
+\\XSS反射演示
+<form action="" method="get">
+    <input type="text" name="xss"/>
+    <input type="submit" value="test"/>
+</form>
+<?php
+$xss = @$_GET['xss'];
+if($xss!==null){
+    echo $xss;
+}
